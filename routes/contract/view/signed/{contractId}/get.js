1d2d32f2e385f96328814c1a0113909ed3ee2f6f
zhuzf <zefu.zhu@dianrong.com>
2018-06-20 13:51:48 +0800

    first commit

--- routes/contract/view/signed/{contractId}/get.js
+++
@@ -1,9 +0,0 @@
-/**
-    合同预览（至少一方已签署）
-  **/
-  const Mock = require("mockjs");
-  module.exports = function (app) {
-    app.get('/api/contract/view/signed/:contractId', (req, res) => {
-      res.json(Mock.mock("@string"));
-    });
-  };
--- routes/file/readImage/get.js
+++
@@ -1,9 +0,0 @@
-/**
-    文件回显用，通过token来获取文件
-  **/
-  const Mock = require("mockjs");
-  module.exports = function (app) {
-    app.get('/api/file/readImage', (req, res) => {
-      res.json(Mock.mock(undefined));
-    });
-  };
