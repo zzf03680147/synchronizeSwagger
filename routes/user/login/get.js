ca46328a379679fda92c99a66a0f069dc9499a74
zhuzf <zefu.zhu@dianrong.com>
2018-06-22 13:29:34 +0800

    update

--- routes/user/login/get.js
+++
@@ -1,9 +0,0 @@
-/**
-      Logs user into the system
-    **/
-    const Mock = require("mockjs");
-    module.exports = function (app) {
-      app.get('/api/user/login', (req, res) => {
-        res.json(Mock.mock("@string"));
-      });
-    };
